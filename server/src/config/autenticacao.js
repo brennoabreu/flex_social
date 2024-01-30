2489a8a813b63869418ba6473a659ab8971378c6
Talisson Bruno <talissonbrsouza@gmail.com>
2024-01-29 22:44:51 -0300

    Backend - excluir arquivos .js

--- server/src/config/autenticacao.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    jwt: {
-        secret: '0ffc7add3858ea3bdf27534a852df935',
-        expiresIn: '1d',
-    }
-};
